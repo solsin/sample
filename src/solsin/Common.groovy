bb8baabe6cb90c28da09878ea7a546e587c2cb49
solsin <charles@plgrim.com>
2020-05-08 18:08:29 +0900

    .

--- src/solsin/Common.groovy
+++ src/solsin/Common.groovy
@@ -2,14 +2,15 @@
 package solsin
 
 def checkoutSCM(String branch) {
+    /**
 	return {
-	/**
+
         stage("checkout") {
             git branch: gitBranch, credentialsId: 'glyde-codecommit-admin', url: 'https://git-codecommit.ap-northeast-2.amazonaws.com/v1/repos/glyde-mall-develop'             
         }
-        */
-        git branch: gitBranch, credentialsId: 'glyde-codecommit-admin', url: 'https://git-codecommit.ap-northeast-2.amazonaws.com/v1/repos/glyde-mall-develop'
-	}
+    }
+    */
+	git branch: gitBranch, credentialsId: 'glyde-codecommit-admin', url: 'https://git-codecommit.ap-northeast-2.amazonaws.com/v1/repos/glyde-mall-develop'
 }
 
 return this
