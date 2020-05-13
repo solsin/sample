5cc831c1c84c632bd97a519bcc7966c363a818bf
solsin <charles@plgrim.com>
2020-05-13 17:31:54 +0900

    .

--- src/solsin/Common.groovy
+++ src/solsin/Common.groovy
@@ -9,7 +9,7 @@ def checkoutSCM(String branch) {
 	git branch: branch, credentialsId: 'glyde-codecommit-admin', url: 'https://git-codecommit.ap-northeast-2.amazonaws.com/v1/repos/glyde-mall-develop'
 	sh "git tag ${VERSION_NUMBER}"
     withCredentials([
-        usernamePassword(credentialsId: 'github', usernameVariable: 'GIT_USERNAME', passwordVariable: 'GIT_PASSWORD')
+        usernamePassword(credentialsId: 'github-solasin', usernameVariable: 'GIT_USERNAME', passwordVariable: 'GIT_PASSWORD')
     ]) {
         sh "git push origin ${VERSION_NUMBER}"
     }
